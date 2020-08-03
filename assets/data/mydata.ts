aad3b4b025aca5e1eae709ca808f18b5c92f992e
Arjun <65750208+patelarj@users.noreply.github.com>
2020-08-03 00:28:54 -0400

    Add files via upload

---
+++ assets/data/mydata.ts
@@ -0,0 +1,11 @@
+import {MyInfoClass} from "../../app/patelarj";
+
+export const MYINFO: MyInfoClass ={
+ 
+    snum: '991311699',
+    sname: 'Arjun Patel',
+    slogname: 'patelarj',
+    scamp: 'DAVIS',
+    assin: 'Assingment_4'
+    
+}
